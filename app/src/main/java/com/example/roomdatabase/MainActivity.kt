37def00e4566298630d0fcc92245196027d3c331
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2023-01-30 16:29:52 +0400

    worked on basic room data base functionality

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        database = Room.databaseBuilder(applicationContext,
+        ContactDatabase::class.java,
+        "contactDB").build()
+
+        GlobalScope.launch {
+            database.contactDao().insertContact(Contact(0,"Developer","9999"))
+        }
+
+    }
+
+    fun addData(view: View) {
+        database.contactDao().getContacts().observe(this, Observer {
+            Log.d("MAIN", it.toString())
+        })
     }
 }
